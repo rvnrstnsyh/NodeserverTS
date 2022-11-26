92f51305808d410abbd29333346f53c8a013a35d
rvnrstnsyh <re@rvnrstnsyh.dev>
2022-11-27 04:01:55 +0700

    update: dependencies

--- ignite.sh
+++ ignite.sh
@@ -10,42 +10,45 @@
 #!/bin/sh
 
 install(){
-  { # Catch
-  npm install \
-              compression@latest \
-              connect-flash@latest \
-              cookie-parser@latest \
-              cors@latest \
-              dotenv@latest \
-              envalid@latest \
-              express@latest \
-              express-rate-limit@latest \
-              express-session@latest \
-              helmet@latest \
-              module-alias@latest \
-              joi@latest \
-              mongoose@latest \
-              morgan@latest && \
+  { # Dependencies
+    npm install \
+                compression@latest \
+                connect-flash@latest \
+                cookie-parser@latest \
+                cors@latest \
+                csrf@latest \
+                csurf@latest \
+                dotenv@latest \
+                envalid@latest \
+                express@latest \
+                express-rate-limit@latest \
+                express-session@latest \
+                helmet@latest \
+                joi@latest \
+                module-alias@latest \
+                mongoose@latest \
+                morgan@latest && \
 
-  npm install \
-              @types/compression --save-dev \
-              @types/connect-flash --save-dev \
-              @types/cookie-parser --save-dev \
-              @types/cors --save-dev \
-              @types/express --save-dev \
-              @types/express-session --save-dev \
-              @types/morgan --save-dev \
-              @types/node --save-dev \
-              @typescript-eslint/eslint-plugin --save-dev \
-              @typescript-eslint/parser --save-dev \
-              eslint --save-dev@latest \
-              eslint-config-prettier@latest --save-dev \
-              eslint-plugin-prettier@latest --save-dev \
-              prettier@latest --save-dev \
-              tsc-watch@latest --save-dev \
-              typescript@latest --save-dev && \
+    npm install \
+                @types/compression --save-dev \
+                @types/connect-flash --save-dev \
+                @types/cookie-parser --save-dev \
+                @types/cors --save-dev \
+                @types/csurf --save-dev \
+                @types/express --save-dev \
+                @types/express-session --save-dev \
+                @types/morgan --save-dev \
+                @types/node --save-dev \
+                @typescript-eslint/eslint-plugin --save-dev \
+                @typescript-eslint/parser --save-dev \
+                eslint@latest --save-dev \
+                eslint-config-prettier@latest --save-dev \
+                eslint-plugin-prettier@latest --save-dev \
+                prettier@latest --save-dev \
+                tsc-watch@latest --save-dev \
+                typescript@latest --save-dev && \
 
-  cp .env.example .env && yarn dev
+    cp .env.example .env && yarn dev
  }
 }
 
