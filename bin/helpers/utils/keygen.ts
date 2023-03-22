65dd9b7d86dd891d1a4b17ce7c06b424f3b68efa
Rivane Rasetiansyah <re@rvnrstnsyh.dev>
2023-03-22 15:35:45 +0700

    build: project setup installation

---
+++ bin/helpers/utils/keygen.ts
@@ -0,0 +1,33 @@
+const elliptic = require('elliptic')
+const CryptoJS = require('crypto-js')
+
+const secp256k1 = new elliptic.ec('secp256k1')
+
+class ApikeyFactory {
+  /**
+   *  !-- GENERATE KEY PAIR (Method)
+   *
+   * @desc generate key pair.
+   * @return object
+   */
+  static genKey(length: number): object {
+    //
+    const format: string = '0123456789ABCDEF'
+    const randKey: Array<string> = ['']
+
+    for (let i: number = length; i > 0; --i) randKey[0] += format[Math.floor(Math.random() * format.length)]
+
+    const apikey: string = randKey[0]
+    const privatekey: object | any = CryptoJS.SHA256(apikey).toString()
+    const keypair: object | any = secp256k1.keyFromPrivate(privatekey)
+    const publickey: object = {
+      original: keypair.getPublic('hex'),
+      compressed: keypair.getPublic(true, 'hex'),
+      x: keypair.getPublic().getX().toString('hex'),
+      y: keypair.getPublic().getY().toString('hex')
+    }
+    return { apikey, privatekey, publickey }
+  }
+}
+
+export default ApikeyFactory
